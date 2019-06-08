933151273907613b9d4825d5ca70b3469ddf65e2
David Little <littledavid175@gmail.com>
2019-06-08 19:45:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// add solution here
+function theBeatlesPlay(musicians,instruments){
+var array=[]
+  for(let i=0;i<musicians.length;i++){
+  array.push(`${musicians[i]} plays ${instruments[i]}`)
+  }
+  return array
+}
+function johnLennonFacts(array){
+  while(array<array.length){
+    array.push(`${array}!!!`)
+  }return array
+}
+function iLoveTheBeatles(n){
+  var last=[]
+  var i = 0
+   if (n < 17){
+     last.push("I love the Beatles!")
+   }
+   else {
+  do { 
+    console.log(last.push("I love the Beatles!"))
+  }
+  while (i < 17)
+   }
+  return last.push("I love the Beatles!")
+}
